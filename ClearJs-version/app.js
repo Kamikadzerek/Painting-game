14f1ae6fd7770d543b26282a7e07a04285e3b2ca
Kamikadzerek <szymekkaczmarczyk@gmail.com>
2020-06-17 19:07:27 +0200

    add save and restor features

--- ClearJs-version/app.js
+++ ClearJs-version/app.js
@@ -12,9 +12,8 @@ function generateMatrix(){
             div.addEventListener("click", function() {
                 index = colors.indexOf(rgbToHex(this.style.background));
                 this.style.background = index == colors.length-1 ? colors[0] : colors[index+1]; 
-                
             } )
-            div.id = i.toString() + j.toString();
+            div.id = j.toString() + i.toString();
             document.getElementById("main").appendChild(div);
         }
       } 
@@ -25,7 +24,33 @@ function rgbToHex(rgb) {
     return '#' + parseInt(rgb[0]).toString(16) + parseInt(rgb[1]).toString(16) + parseInt(rgb[2]).toString(16);
 }
 
+function generateJsonArray() {
+    let cells = document.querySelectorAll('.cell');
+    var jsonArray = [];
+    for (let i = 0; i< cells.length; i++) {
+        let  item = {
+            "x": cells[i].id[0],
+            "y": cells[i].id[1],
+            "color": rgbToHex(cells[i].style.background)
+        }
+    
+        jsonArray.push(item);
+    }
+    return jsonArray;
+}
 
 
+function sendToLocalStorage() {
+    localStorage.setItem('cells', JSON.stringify(generateJsonArray()));
+}
+function restorFromLocalStorage() {
+    let cells = JSON.parse(localStorage.getItem('cells'));
+    for (let i = 0; i< cells.length; i++) {
+        document.getElementById(cells[i].x + cells[i].y).style.background=cells[i].color;
 
-generateMatrix()
+    }
+}
+
+generateMatrix();
+document.querySelector('#save').addEventListener('click', sendToLocalStorage)
+document.querySelector('#restor').addEventListener('click', restorFromLocalStorage)
